07a4c3f9d71027e314048294eab1bac33bce3964
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-10 20:46:37 +0600

    the change add file text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 
 
 import "./App.css";
-
+import Example  from "./components/Example/Example"
+import Button  from "./components/Button/Button"
+import Maksim1  from "./components/Maksim1/Maksim1"
+import Maksim2  from "./components/Maksim2/Maksim2"
+import Maksim3  from "./components/Maksim3/Maksim3"
+import Limon  from "./components/Limon/Limon"
+import Efratx  from "./components/Efratx/Efratx"
+import MyWife  from "./components/MyWife/MyWife"
+import MyFrends from "./components/MyFrends/MyFrends";
+import Alinur from "./components/Alinur/Alinur";
 function App() {
   return (
     <div className="App">
       
 heloo world
-   
-    </div>
+
+<Example>Hello world</Example>
+   <Button >Hello</Button>
+   <br></br>
+   <Maksim1>Hello</Maksim1>
+   <br></br>
+   <Maksim2>Hello</Maksim2>
+    
+     <Maksim3>Hello</Maksim3>
+     <br></br>
+     <Limon>Hello</Limon>
+     <br></br>
+     <Efratx>Hello</Efratx>
+     <br></br>
+     <MyWife>Hello</MyWife>
+     <br></br>
+     <MyFrends>Hello</MyFrends>
+     <br></br>
+     <Alinur>Hello</Alinur>
+     </div>
   );
 }
 
